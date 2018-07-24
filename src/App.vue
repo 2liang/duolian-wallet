02a3dba7b7913acae82316f2864b9af7cda3c93b
2liang <liangwl@chuchujie.com>
2018-07-24 20:50:36 +0800

    init 创建账户代码

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
                     background-color="#001529"
                     text-color="#fff"
                     active-text-color="#fff">
-                    <li class="el-menu-item" style="color: rgb(255, 255, 255); border-bottom-color: transparent; background-color: rgb(0, 21, 41);" id="logo">破可乐瓶的钱包</li>
+                    <li class="el-menu-item" style="color: rgb(255, 255, 255); border-bottom-color: transparent; background-color: rgb(0, 21, 41);" id="logo">多链钱包</li>
                     <el-menu-item index="/"><i class="el-icon-menu" style="margin-bottom: 3px;"></i>首页</el-menu-item>
                     <el-menu-item index="/account/info"><i class="el-icon-view" style="margin-bottom: 3px;"></i>我的账户</el-menu-item>
                     <el-menu-item index="/transaction/receive"><i class="el-icon-success" style="margin-bottom: 3px;"></i>我要交易</el-menu-item>
@@ -25,7 +25,7 @@
     </div>
    
     <div id="footer" style="margin: 50px 0;">
-        <span>Wallet@2018 破可乐瓶</span>
+        <span>Wallet@2018 多链科技</span>
     </div>
   </div>
 </template>
--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>    
-        <h1>创建账号</h1>
+    <div class="create-account-content">    
+        <div class="primary">
+            <el-card class="box-card create-account-card">
+            <div class="text item">
+                <el-button ref="createBtn" type="primary" v-on:click="createBtnClick">{{ createBtnMessage }}</el-button>
+                <div style="margin-top: 30px; height: 50px; line-height: 50px;" ref="mnemonicDom"></div>
+                <div style="height: 50px; line-height: 50px; color: red;" ref="mnemonicTip"></div>
+                <el-button class="removeBtn" style="display: block;" ref="removeBtn" type="info" v-on:click="removeBtnClick">清除本地账号</el-button>
+            </div>
+            </el-card>
+        </div> 
     </div>
 </template>
 
@@ -8,10 +17,64 @@
 
 export default {
 
+    data: function () {
+        return {
+            createBtnMessage: "点击生成助记词",
+        }
+    },
+
+    created: function () {
+        if (!window.localStorage) {
+            this.$message({
+                message: '您的浏览器不支持LocalStorage，请升级您的浏览器。',
+                type: 'error'
+            }); 
+            
+        }
+    },
+
+    mounted: function() {
+        this.checkAccount();
+    },
+
+    methods: {  
+
+        // 检测本地是否有账号
+        checkAccount() {
+            
+        },
+
+        // 生成助记词
+        generateMnemonic() {
+            
+        },
+
+        // 创建按钮绑定点击事件
+        createBtnClick() {
+
+        },
+
+        // 移除按钮绑定点击事件
+        removeBtnClick() {
+
+        }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .create-account-card {
+        width: 75%;
+        min-width: 800px;
+        margin: 0 auto;
+        margin-top: 50px;
+        margin-left: 30px;
+        min-height: 300px;
+    }
 
+    .removeBtn {
+        float: right;
+        margin-top: 50px;
+    }
 </style>
